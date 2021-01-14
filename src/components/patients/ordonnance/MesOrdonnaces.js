88d87aaced6c8df060d72c90b4956defd23b1190
aguezzi <guezziadil@gmail.com>
2021-01-14 23:32:10 +0100

    add doc filter

--- src/components/patients/ordonnance/MesOrdonnaces.js
+++ src/components/patients/ordonnance/MesOrdonnaces.js
@@ -4,7 +4,10 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import Typography from '@material-ui/core/Typography';
-import { FaAngleDown, FaCheckCircle} from 'react-icons/fa';
+import { FaAngleDown, FaCheckCircle, FaCalendarPlus} from 'react-icons/fa';
+
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import "./mesOrdonnaces.scss";
 import HeaderComponent from '../header/headerComponent'
@@ -28,9 +31,17 @@ export default function MesOrdonnaces() {
 
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  const [filter, setFilter] = React.useState('Generalist');
 
+  const handleChangeTab = (event, newValue) => {
+    setValue(newValue);
+  };
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
+  };
+  const handleChangeFilter = (panel) => (event, isExpanded) => {
+    setFilter(panel);
   };
 
   return (
@@ -41,8 +52,31 @@ export default function MesOrdonnaces() {
           Mes Ordonnances
         </div>
         <div className="container-subtitle">
-          liste des ordonnances pour Mr XXXX XXXX planifiés
+          liste des Consultations pour Mr XXXX XXXX planifiés
         </div>
+      
+
+        <div className="container-filters-top">
+          <div className={filter === 'Generalist' ? "container-filter-top-actif" : "container-filter-top" }
+          onClick={handleChangeFilter('Generalist')}>Généraliste</div>
+          <div className={filter === 'Specialist' ? "container-filter-top-actif" : "container-filter-top" }
+          onClick={handleChangeFilter('Specialist')}>Specialiste</div>
+        </div>
+
+
+        <Tabs
+          value={value}
+          onChange={handleChangeTab}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Terminé" />
+          <Tab label="En cours" />
+          <Tab label="A venir" />
+        </Tabs>
+      {
+        value === 0 ? 
       <div className={classes.root}>
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary
@@ -69,16 +103,13 @@ export default function MesOrdonnaces() {
 
             <div  className="lines__line"> 
               <div className="lines__title">status</div>
-              <div className="lines__last-line-desc"> Pending</div>
-              <div className="lines__last-line-button"> <FaCheckCircle>  </FaCheckCircle>Valider</div>
+              <div className="lines__last-line-desc"> Terminé</div>
             </div>
           </div>
         </div>
         </AccordionDetails>
       </Accordion>
     
-
-
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary
           expandIcon={<FaAngleDown />}
@@ -104,24 +135,28 @@ export default function MesOrdonnaces() {
 
             <div  className="lines__line"> 
               <div className="lines__title">status</div>
-              <div className="lines__last-line-desc"> Pending</div>
-              <div className="lines__last-line-button"> <FaCheckCircle>  </FaCheckCircle>Valider</div>
+              <div className="lines__last-line-desc"> Terminé</div>
             </div>
           </div>
         </div>
         </AccordionDetails>
       </Accordion>
     
+    </div> : null
+      }
 
 
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+{
+        value === 1 ? 
+      <div className={classes.root}>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary
           expandIcon={<FaAngleDown />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography className={classes.heading}>11/02/2020</Typography>
-          <Typography className={classes.secondaryHeading}>Dr Naciri Ibrahim</Typography>
+          <Typography className={classes.heading}>11/02/2019</Typography>
+          <Typography className={classes.secondaryHeading}>Dr DUPONT Jean</Typography>
         </AccordionSummary>
         <AccordionDetails>
         <div>
@@ -139,7 +174,7 @@ export default function MesOrdonnaces() {
 
             <div  className="lines__line"> 
               <div className="lines__title">status</div>
-              <div className="lines__last-line-desc"> Pending</div>
+              <div className="lines__last-line-desc"> En cours</div>
               <div className="lines__last-line-button"> <FaCheckCircle>  </FaCheckCircle>Valider</div>
             </div>
           </div>
@@ -147,16 +182,21 @@ export default function MesOrdonnaces() {
         </AccordionDetails>
       </Accordion>
     
+    </div> : null
+      }
 
 
-      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+{
+        value === 2 ? 
+      <div className={classes.root}>
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary
           expandIcon={<FaAngleDown />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography className={classes.heading}>11/02/2020</Typography>
-          <Typography className={classes.secondaryHeading}>Dr Naciri Ibrahim</Typography>
+          <Typography className={classes.heading}>11/02/2022</Typography>
+          <Typography className={classes.secondaryHeading}>Dr Ait Nali Karim</Typography>
         </AccordionSummary>
         <AccordionDetails>
         <div>
@@ -174,16 +214,16 @@ export default function MesOrdonnaces() {
 
             <div  className="lines__line"> 
               <div className="lines__title">status</div>
-              <div className="lines__last-line-desc"> Pending</div>
-              <div className="lines__last-line-button"> <FaCheckCircle>  </FaCheckCircle>Valider</div>
+              <div className="lines__last-line-desc"> a venir</div>
+              <div className="lines__last-line-button"> <FaCheckCircle>  </FaCheckCircle>Annuler</div>
             </div>
           </div>
         </div>
         </AccordionDetails>
       </Accordion>
     
-    
-    </div>
+    </div> : null
+      }
       </div>
     </div>
   );

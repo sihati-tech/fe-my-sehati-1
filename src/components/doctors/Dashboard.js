6df0210497c857b8732b35c208d45d589f966ff7
aguezzi <guezziadil@gmail.com>
2021-07-01 22:19:48 +0200

    env

--- src/components/doctors/Dashboard.js
+++ src/components/doctors/Dashboard.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import axiosInstance from '../../services/httpInterceptor' 
 import "./Dashboard.scss";
 import axios from 'axios';
 
